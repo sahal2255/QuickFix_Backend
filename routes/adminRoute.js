9d05448191ad8598782c2d42cd989e2c2611261b
sahal2255 <sahalvv9656@gmail.com>
2025-02-04 14:06:12 +0530

    route

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,6 +1,6 @@
 const express=require('express')
 const router=express.Router()
-const adminController=require('../controllersController')
+const adminController=require('../controllers/adminController')
 const authMiddleware =require('../middleware/authMiddleware')
 const multer=require('multer')
 const upload = multer();
